59d3e7d364e75113ea1687dfc831a70e318e183f
Nathan Nguyen <nathannguyen@catalyst-au.net>
2018-09-10 10:09:11 +1000

    Tab topic course format - add GDPR.

--- lang/en/format_tabtopics.php
+++ lang/en/format_tabtopics.php
@@ -31,3 +31,4 @@ $string['tabtopics_zero_as_tab'] = 'Zero Section As Tab';
 $string['tabtopics_zero_as_tab_help'] = 'Should section 0 of the course be a tab? If no, then it will be displayed at the top of the course at all times.';
 $string['tabtopics_remember_last_tab_session'] = 'Return user to previous selected tab';
 $string['tabtopics_remember_last_tab_session_help'] = 'If yes user will return to previous selected tab instead of the first tab.';
+$string['privacy:metadata'] = 'Tab topic course format does not store any personal data.';
